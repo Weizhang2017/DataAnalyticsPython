e8691959a98e8f551466b9e535a71725fec874cb
Michael Bögli <michael.boegli@gmail.com>
2019-10-20 19:04:05 +0800

    fixed bug in firstSubmission

--- michael_code_snippet/round2_firstSubmission.py
+++ michael_code_snippet/round2_firstSubmission.py
@@ -13,4 +13,4 @@ df_output = orders.copy()
 df_output.drop(columns=['buyer_userid', 'seller_userid'], inplace=True)
 df_output['is_fraud'] = 'not fraud'
 
-df_output.set_index('orderid').rename(columns={'name': 'is_fraud'}.to_csv('solution_round2_firstSubmission.csv')
+df_output.set_index('orderid').rename(columns={'name': 'is_fraud'}).to_csv('solution_round2_firstSubmission.csv')
