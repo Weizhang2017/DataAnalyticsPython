64e796976b6b22e3a1603bb39994233a25fc7164
MyBeta <michael.boegli@gmail.com>
2019-10-20 11:18:48 +0800

    categories to groups

--- michael_code_snippet/test.py
+++ michael_code_snippet/test.py
@@ -1,3 +1,23 @@
 import numpy as np
 import pandas as pd 
 
+
+extra2 = pd.read_clipboard('Extra Material 2 - keyword list_with substring.csv') 
+extra2 = pd.read_csv('Extra Material 2 - keyword list_with substring.csv')
+extra3 = pd.read_csv('Extra Material 3 - mismatch list.csv') 
+
+
+extra2['aslist'] = extra2.Keywords.apply(lambda st : st.split(', ')) 
+
+def find_group_id(st = ""): 
+    result = []
+    for index_id, list_of_items in extra2['aslist'].items() : 
+        found = [ kk for kk in list_of_items if kk in st.lower() ]
+        if len(found) > 0: 
+            result.append(index_id)
+    
+    return result
+
+
+keywords_part.name.apply(find_group_id)
+
